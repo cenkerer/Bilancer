82ebb5ec7041459383cd24b50933503a521e547e
zxyctn <zxyctn@gmail.com>
2017-03-25 10:20:50 +0200

    Update Person.java

--- src/Person.java
+++ src/Person.java
@@ -1,15 +1,21 @@
 import java.util.ArrayList;
 
 public class Person {
-	//AVATAR (add when you learn how)
+	private Avatar myAvatar;
 	private String fName, mName, lName;
 	private ArrayList <Subject> myInterests;
 	private Contact myContact;
+	private ArrayList <Experience> pastExperiences;
 	private int bilkentID;
 	private boolean isLearner = false;
 	private boolean isTutor = false;
+	private double rating;
+	private int count;
 	
-	public Person(String fName, String mName, String lName, boolean isLearner, boolean isTutor, int bilkentID) {
+	public Person(Avatar avatar, String fName, String mName, String lName, boolean isLearner, boolean isTutor, int bilkentID) {
+		myAvatar = avatar;
+		rating = 0;
+		count = 0;
 		this.fName = fName;
 		this.mName = mName;
 		this.lName = lName;
@@ -18,16 +24,26 @@ public class Person {
 		this.bilkentID = bilkentID;
 		myContact = new Contact();
 		myInterests = new ArrayList <Subject>();
+		pastExperiences = new ArrayList <Experience>();
 	}
 	
-	public Person(String fName, String lName, boolean isLearner, boolean isTutor, int bilkentID) {
+	public Person(Avatar avatar, String fName, String lName, boolean isLearner, boolean isTutor, int bilkentID) {
+		rating = 0;
+		count = 0;
+		myAvatar = avatar;
 		this.fName = fName;
-		this.lName = lName;A
+		this.lName = lName;
 		this.isLearner = isLearner;
 		this.isTutor = isTutor;
 		this.bilkentID = bilkentID;
 		myContact = new Contact();
 		myInterests = new ArrayList <Subject>();
+		pastExperiences = new ArrayList <Experience>();
+	}
+	
+	public void addRating(double rate) {
+		rating += rate;
+		rating /= ++count;
 	}
 	
 	public void addContact(String phone, String email) {
@@ -38,5 +54,7 @@ public class Person {
 		myInterests.add(interest);
 	}
 	
-	
+	public void addExperience(Experience e) {
+		pastExperiences.add(e);
+	}
 }
